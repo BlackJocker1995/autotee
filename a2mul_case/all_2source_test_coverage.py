becf90461d63a20de5629d03aec8670af930ece2
rain <han784851198@live.com>
2025-01-13 16:08:15 +0800

    add coverage test

---
+++ a2mul_case/all_2source_test_coverage.py
@@ -0,0 +1,31 @@
+import os
+
+from loguru import logger
+
+from LLM.llmodel import OpenAIModel
+from build.build_assistance import TestAssistance
+from static.projectUtil import list_directories
+import numpy as np
+
+if __name__ == '__main__':
+    # codescan = OllamaModel("qwen2.5-coder:32b")
+    base_path = "/home/rdhan/data/dataset/python_mul_case"
+
+    overwrite = False
+
+    dirs = list_directories(base_path)
+
+    average = []
+    
+    for dir_item in dirs:
+        logger.info(f"Switch to {dir_item}.")
+
+        project_path = os.path.join(base_path, dir_item)
+
+        test_assistance = TestAssistance.class_generator("python")
+
+        test_assistance.run_test_coverage(project_path)
+        
+        average.append(test_assistance.calculate_average_coverage(project_path))
+    
+    print(np.mean(average))
